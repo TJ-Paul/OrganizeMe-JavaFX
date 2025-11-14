72c8578b6f828fa47d9ed6a755d36eeaf04b9080
Turjjo <163424914+TJ-Paul@users.noreply.github.com>
2025-11-14 11:40:16 +0600

    Delete src directory

--- src/NavigateController.java
+++
@@ -1,6 +0,0 @@
-import java.io.IOException;
-
-public interface NavigateController {
-	void navigateTo(String fxmlPath, int width, int height) throws IOException;
-    void navigateTo(String fxmlPath) throws IOException;
-}
